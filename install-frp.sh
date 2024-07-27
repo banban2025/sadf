538b2dd067afeb4d8aeccf3795b3744a2eca40af
www <www@www.com>
2024-07-27 17:39:08 +0800

    x2d

--- install-frp.sh
+++ install-frp.sh
@@ -6,7 +6,7 @@ mkdir -p frp
 pushd frp
 
 if [ ! -x frps ]; then
-    wget -qOfrp.tgz https://github.com/fatedier/frp/releases/download/v0.59.0/frp_0.59.0_darwin_amd64.tar.gz
+    wget -qOfrp.tgz https://github.com/fatedier/frp/releases/download/v0.59.0/frp_0.59.0_linux_amd64.tar.gz
     tar xf frp.tgz --strip-components=1
 fi
 
