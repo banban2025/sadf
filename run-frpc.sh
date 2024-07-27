81bf92d5a67a593b7481ea1d4eb99be21d27170f
www <www@www.com>
2024-07-27 17:56:15 +0800

    x2d

--- run-frpc.sh
+++ run-frpc.sh
@@ -11,8 +11,8 @@ EOF
 fi
 
 if [ -v FRPS_PORT ]; then
-  sed -i 's/16000/$FRPS_PORT/g' frpc.ini
-  sed -i 's/ssh/ssh_$FRPS_PORT/g' frpc.ini
+  sed -i "s/16000/$FRPS_PORT/g" frpc.ini
+  sed -i "s/ssh/ssh_$FRPS_PORT/g" frpc.ini
 fi
 
 ./frp/frpc -c frpc.ini 2>&1 | sed -E 's,[0-9\.]+:6969,***:6969,ig' || true
